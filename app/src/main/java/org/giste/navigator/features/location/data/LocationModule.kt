fbf4b14d5e78289f36be7cd13df238d9aa515718
Giste <giste.trappiste@gmail.com>
2025-02-21 17:49:41 +0100

    Add location module

---
+++ app/src/main/java/org/giste/navigator/features/location/data/LocationModule.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2025 Giste Trappiste
+ *
+ * This program is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.giste.navigator.features.location.data
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import org.giste.navigator.features.location.domain.LocationRepository
+import org.giste.navigator.features.settings.domain.SettingsRepository
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+class LocationModule {
+    @Singleton
+    @Provides
+    fun provideLocationRepository(
+        settingsRepository: SettingsRepository,
+        @ApplicationContext context: Context,
+    ): LocationRepository = LocationManagerLocationRepository(settingsRepository, context)
+}
--- app/src/main/java/org/giste/navigator/ui/theme/Theme.kt
+++ app/src/main/java/org/giste/navigator/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package org.giste.navigator.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
